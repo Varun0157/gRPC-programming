bb02297a06aad243a12eb93c7edb87856f4ccb2b
Varun0157 <varun.edachali@gmail.com>
2024-10-16 00:28:02 +0530

    feat: request timeout handled by client driver

--- src/3 myUber/client/driver.go
+++ src/3 myUber/client/driver.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
@@ -11,12 +14,75 @@ import (
 	comm "distsys/grpc-prog/myuber/comm"
 )
 
+func getRequestTimeout() time.Duration {
+	return 10 * time.Second
+}
+
 func getDriverDetails() (name string) {
 	fmt.Println("Enter your name: ")
 	fmt.Scan(&name)
 
 	return name
 }
+
+func rejectRide(client comm.DriverServiceClient, rideId int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), getRequestTimeout())
+	defer cancel()
+	_, err := client.RejectRideRequest(ctx, &comm.DriverRejectRequest{
+		RideId: int32(rideId),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to reject ride: %v", err)
+	}
+
+	return nil
+}
+
+func acceptRide(client comm.DriverServiceClient, rideId int, name string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), getRequestTimeout())
+	defer cancel()
+
+	_, err := client.AcceptRideRequest(ctx, &comm.DriverAcceptRequest{
+		RideId: int32(rideId),
+		Driver: name,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to accept ride: %v", err)
+	}
+
+	return nil 
+}
+
+func completeRide(client comm.DriverServiceClient, rideId int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), getRequestTimeout())
+	defer cancel()
+
+	_, err := client.CompleteRideRequest(ctx, &comm.DriverCompleteRequest{
+		RideId: int32(rideId),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to complete ride: %v", err)
+	}
+
+	return err 
+}
+
+func timeoutHit(client comm.DriverServiceClient, rideId int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), getRequestTimeout())
+	defer cancel()
+
+	_, err := client.TimeoutRideRequest(ctx, &comm.DriverTimeoutRequest{
+		RideId: int32(rideId),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to timeout ride: %v", err)
+	}
+
+	return err
+}
+
+const WAIT_TIME = 10
 
 func connectDriver(port int) error {
 	conn, err := grpc.NewClient(fmt.Sprintf(":%d", port), grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -29,7 +95,7 @@ func connectDriver(port int) error {
 	name := getDriverDetails()
 
 	for {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), getRequestTimeout())
 		rideResponse, err := client.AssignDriver(ctx, &comm.DriverAssignmentRequest{
 			Driver: name,
 		})
@@ -41,43 +107,53 @@ func connectDriver(port int) error {
 		fmt.Println(rideResponse.RideId)
 
 		var choice string
-		fmt.Println("Do you want to accept or reject ride? (a/r)")
-		fmt.Scan(&choice)
+		
+		ctx, cancel = context.WithTimeout(context.Background(), WAIT_TIME * time.Second)
+		defer cancel()
+		inputChan := make(chan string)
 
-		if choice == "r" {
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			_, err := client.RejectRideRequest(ctx, &comm.DriverRejectRequest{
-				RideId: int32(rideResponse.RideId),
-			})
-			cancel()
-			if err != nil {
-				return fmt.Errorf("failed to reject ride: %v", err)
+		go func() {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("Do you want to accept or reject ride? (a/r)")
+			text, _ := reader.ReadString('\n')
+
+			select {
+			case inputChan <- text:
+			case <-ctx.Done():
 			}
+		}()
 
+		select {
+		case choice = <-inputChan:
+		case <-ctx.Done():
+			err = timeoutHit(client, int(rideResponse.RideId))
+			if err != nil {
+				return err
+			}
 			continue
 		}
 
-		ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
-		_, err = client.AcceptRideRequest(ctx, &comm.DriverAcceptRequest{
-			RideId: int32(rideResponse.RideId),
-			Driver: name,
-		})
-		cancel()
+		choice = strings.Trim(choice, "\n")
+		if choice == "r" {
+			err = rejectRide(client, int(rideResponse.RideId))
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		print("here")
+
+		err = acceptRide(client, int(rideResponse.RideId), name)
 		if err != nil {
-			return fmt.Errorf("failed to accept ride: %v", err)
+			return err
 		}
 
-		fmt.Println("Press anything to complete ride")
-		fmt.Scan(&choice)
-		ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
-		_, err = client.CompleteRideRequest(ctx, &comm.DriverCompleteRequest{
-			RideId: int32(rideResponse.RideId),
-		})
-		cancel()
+		fmt.Println("press the enter key to complete ride")
+		fmt.Scanln()
+		err = completeRide(client, int(rideResponse.RideId))
 		if err != nil {
-			return fmt.Errorf("failed to complete ride: %v", err)
+			return err
 		}
-		break
 	}
 	return nil
 }
